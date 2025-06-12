52b36a1a34b9318e1bc5ef832417e90636200320
Philip <philipwinduong@gmail.com>
2025-06-11 22:54:10 -0700

    Add neovim and iTerm2 configs

---
+++ neovim/init.lua
@@ -0,0 +1,222 @@
+-- This section automatically downloads and installs lazy.nvim if it's not already present
+-- Think of this as ensuring the "app store" is installed before we try to download apps
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Set up the leader key before loading plugins
+-- The leader key is like a modifier key for custom shortcuts (similar to Ctrl+Shift in VSCode)
+vim.g.mapleader = " " -- Using spacebar as the leader key
+
+-- Basic editor settings that make Neovim more familiar coming from VSCode
+vim.opt.number = true              -- Show line numbers (like VSCode's line numbers)
+vim.opt.relativenumber = false     -- Use absolute line numbers, not relative ones
+vim.opt.tabstop = 4               -- Tab width of 2 spaces
+vim.opt.shiftwidth = 4            -- Indentation width of 2 spaces  
+vim.opt.expandtab = true          -- Convert tabs to spaces
+vim.opt.wrap = true              -- Don't wrap long lines
+vim.opt.ignorecase = true         -- Case-insensitive searching
+vim.opt.smartcase = true          -- But case-sensitive if search contains uppercase
+vim.opt.termguicolors = true      -- Enable full color support
+vim.opt.signcolumn = "yes"        -- Always show the sign column (like VSCode's gutter)
+
+-- This is where we'll define our plugins
+-- Think of this as your "extensions" list
+require("lazy").setup({
+  -- Fuzzy file finder - equivalent to VSCode's Ctrl+P
+  {
+    "junegunn/fzf.vim",
+    dependencies = {
+      -- This installs the core fzf binary that the vim plugin uses
+      { "junegunn/fzf", build = ":call fzf#install()" }
+    },
+    -- These are the keyboard shortcuts for the fuzzy finder
+    keys = {
+      { "<C-p>", ":Files<CR>", desc = "Find files" },
+      { "<C-f>", ":Rg<CR>", desc = "Search in files" },
+      { "<leader>b", ":Buffers<CR>", desc = "Find open buffers" },
+    },
+  },
+
+  -- Molokai theme here
+  {
+    "tomasr/molokai", -- This is the GitHub repository for the molokai theme
+    name = "molokai",  -- Give it a friendly name for lazy.nvim to reference
+    priority = 1000,   -- Load this plugin early (themes should load before other plugins)
+    config = function()
+      -- This function runs after the plugin loads and activates the theme
+      vim.cmd.colorscheme("molokai") -- This is equivalent to running :colorscheme molokai
+    end,
+  },
+
+  -- File explorer - like VSCode's file sidebar
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" }, -- Pretty file icons
+    config = function()
+      -- This function runs after the plugin is loaded and sets it up
+      require("nvim-tree").setup({
+        view = {
+          width = 30, -- Sidebar width
+        },
+        renderer = {
+          group_empty = true, -- Group empty folders
+        },
+      })
+    end,
+    keys = {
+      { "<leader>e", ":NvimTreeToggle<CR>", desc = "Toggle file explorer" },
+    },
+  },
+
+  -- Syntax highlighting - makes code colorful and properly formatted
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate", -- Automatically update language parsers
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        -- Install parsers for these languages automatically
+        ensure_installed = { "lua", "python", "javascript", "typescript", "html", "css" },
+        highlight = { enable = true }, -- Enable syntax highlighting
+        indent = { enable = true }, -- Enable smart indentation
+      })
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "ts_ls", "lua_ls" },
+      })
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = { 
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+      local lspconfig = require("lspconfig")
+
+      -- Shared on_attach function to avoid repetition
+      local function on_attach(client, bufnr)
+        local bufopts = { noremap=true, silent=true, buffer=bufnr }
+
+        -- LSP keymaps
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+        vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+
+        -- Diagnostic keymaps
+        vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, bufopts)
+        vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, bufopts)
+        vim.keymap.set('n', ']d', vim.diagnostic.goto_next, bufopts)
+        vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, bufopts)
+      end
+
+      -- TypeScript setup
+      lspconfig.ts_ls.setup({
+        on_attach = on_attach,
+      })
+
+      -- Lua setup
+      lspconfig.lua_ls.setup({
+        on_attach = on_attach,
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = {'vim'}
+            }
+          }
+        }
+      })
+    end,
+  },
+
+  -- Autocompletion
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",    -- LSP source for nvim-cmp
+      "hrsh7th/cmp-buffer",      -- Buffer completions
+      "hrsh7th/cmp-path",        -- Path completions
+      "hrsh7th/cmp-cmdline",     -- Command line completions
+      "L3MON4D3/LuaSnip",       -- Snippet engine
+      "saadparwaiz1/cmp_luasnip", -- Snippet completions
+    },
+    config = function()
+      local cmp = require('cmp')
+      local luasnip = require('luasnip')
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },    -- LSP completions (this is the important one!)
+          { name = 'luasnip' },     -- Snippet completions
+        }, {
+          { name = 'buffer' },      -- Buffer completions
+          { name = 'path' },        -- Path completions
+        })
+      })
+    end,
+  },
+})
+
+-- Basic keymaps that feel familiar from other editors
+vim.keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+vim.keymap.set("n", "<C-s>", ":w<CR>", { desc = "Save file with Ctrl+S" })
+
