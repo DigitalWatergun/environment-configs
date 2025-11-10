8ff75c99df7318aa418f04885ad18bd5ecfbe352
DigitalWatergun <digitalwatergun@gmail.com>
2025-11-10 11:46:33 -0800

    Rename config files

--- nvim/minimal-white-blue-init.lua
+++ nvim/minimal-white-blue-init.lua
@@ -1559,7 +1559,7 @@ require("lazy").setup({
 			require("lualine").setup({
 				options = {
 					theme = custom_theme,
-					component_separators = { left = '', right = '' },
+					component_separators = { left = "", right = "" },
 					section_separators = "",
 				},
 				sections = {
