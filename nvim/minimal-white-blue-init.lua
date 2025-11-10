b8f4e5895eff7bad7b34b5ed545d0a39d154f12b
DigitalWatergun <digitalwatergun@gmail.com>
2025-11-10 14:43:50 -0800

    Ran formatter

--- nvim/minimal-white-blue-init.lua
+++ nvim/minimal-white-blue-init.lua
@@ -1552,7 +1552,7 @@ require("lazy").setup({
 			require("lualine").setup({
 				options = {
 					theme = custom_theme,
-					component_separators = { left = '', right = '' },
+					component_separators = { left = "", right = "" },
 					section_separators = { left = "", right = "" },
 				},
 				sections = {
